1fc028c19686d9751fc8869049ff2adfbac8394a
Newton Bujiku <igotti47@gmail.com>
2015-04-15 16:20:20 +0100

    Rename sendGet method to sendGETRequest Fix issues with sendGETRequest on handling the server response Fix parameters encoding in the query string

--- src/com/gottibujiku/httpjsonexchange/main/HttpJSONExchange.java
+++ src/com/gottibujiku/httpjsonexchange/main/HttpJSONExchange.java
@@ -8,12 +8,11 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.file.attribute.AclEntry.Builder;
 import java.util.HashMap;
 import java.util.Set;
 
+import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONStringer;
 
 /**
  * This class manages the transfer of text based data over the network
@@ -26,7 +25,7 @@ import org.json.JSONStringer;
  * 
  */
 public class HttpJSONExchange {
-
+	private static final char EQUAL = '=';
 	private static final char PARAM_SEPARATOR ='&';//used to separate the parameters in the query string
 	private static final char QUERY_SEPARATOR ='?';//used to separate the domain and the query string
 	private static final String UTF_CHARSET = "UTF-8";
@@ -34,33 +33,56 @@ public class HttpJSONExchange {
 	public HttpJSONExchange(){
 
 	}
+	
+	/**
+	 * This method sends a GET request to the server.
+	 * It prepares the request by creating a valid URL with a query string(if any)
+	 * then adds the headers and then open a connection to the server; retrieves
+	 * response and return result as a JSON Object
+	 * 
+	 * 
+	 * @param fullDomainName a fully qualified domain name including the protocol
+	 * @param queryParams    parameters to be sent in the query string
+	 * @param headers        additional headers
+	 * @return a JSON object containing the response             
+	 */
 
 
-	public JSONObject sendGet(String fullDomainName, HashMap<String, String> queryParams, HashMap<String, String> headers){
+	public JSONObject sendGETRequest(String fullDomainName, HashMap<String, String> queryParams, HashMap<String, String> headers){
+		
+		/* 1. Form the query string by concatenating param names and their values
+		 * 2. Add HTTP headers to the request and set request method to GET
+		 * 3. Open stream and read server response
+		 * 
+		 */
 		JSONObject jsonResponse = null;
 		URL url = null;//a url object to hold a complete URL
+		Set<String> keys = null;
 
 		StringBuilder stringBuilder = new StringBuilder();//avoid strings,they are immutable
+		if(queryParams != null){//do this if there were parameters given
+			//Loop around the map to form a complete query string
+			//Do encoding to escape special characters
+			 keys = queryParams.keySet();//holds all keys in the map
+			String value = null;
+			for(String key : keys){
+				value = queryParams.get(key);//retrieve parameter value
 
-		//Loop around the map to form a complete query string
-		//Do encoding to escape special characters
-		Set<String> keys = queryParams.keySet();//holds all keys in the map
-		String value = null;
-		for(String key : keys){
-			value = queryParams.get(key);//retrieve parameter value
+				try {
+					//encode the  key and value from the map to get name&value pairs and append them to the queryBuilder
+					stringBuilder.append(
+							URLEncoder.encode(key,UTF_CHARSET) + EQUAL +
+							URLEncoder.encode(value,UTF_CHARSET) +PARAM_SEPARATOR
+							);
 
-			try {
-				//encode the  key and value from the map to get name&value pairs and append them to the queryBuilder
-				stringBuilder.append(
-						URLEncoder.encode(key,UTF_CHARSET) + PARAM_SEPARATOR +
-						URLEncoder.encode(value,UTF_CHARSET)
-						);
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+
+				}
 
 			}
-
 		}
+		
 		
 		try {
 			url = new URL(fullDomainName + QUERY_SEPARATOR + stringBuilder.toString());//a complete URL 
@@ -76,7 +98,6 @@ public class HttpJSONExchange {
 			//a call to connection.connect() is superfluous since connect will be called
 			//implicitly when the stream is opened
 			connection.setRequestMethod("GET");
-			
 			String line = null;
 			stringBuilder = new StringBuilder();
 			try(BufferedReader reader =new BufferedReader(new InputStreamReader(connection.getInputStream()))){//open with resources
@@ -85,12 +106,13 @@ public class HttpJSONExchange {
 					while((line = reader.readLine()) != null){
 						//if  the response from the server is not null
 						stringBuilder.append(line);
+
 					}
 				}
 				
 			}
 			connection.disconnect();
-			jsonResponse = new JSONObject(stringBuilder);//change the response into a json object		
+			jsonResponse = new JSONObject(stringBuilder.toString());//change the response into a json object		
 			
 			
 		} catch (MalformedURLException e) {
@@ -99,6 +121,9 @@ public class HttpJSONExchange {
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;//if failed to open a connection
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;//invalid JSON response
 		}
 			
 		return jsonResponse;
