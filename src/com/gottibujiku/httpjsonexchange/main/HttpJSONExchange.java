2610325db3fc81de55719418222202e3ac6e0862
Newton Bujiku <igotti47@gmail.com>
2015-04-20 19:06:03 +0100

    Add a small check for null URLs

--- src/com/gottibujiku/httpjsonexchange/main/HttpJSONExchange.java
+++ src/com/gottibujiku/httpjsonexchange/main/HttpJSONExchange.java
@@ -1,3 +1,24 @@
+
+/*
+    A simple Java library that simplifies
+    exchange of information between Java clients and Http Servers.
+
+    Copyright (C) 2015, Newton Bujiku, igotti47@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.gottibujiku.httpjsonexchange.main;
 
 import java.io.BufferedReader;
@@ -60,6 +81,9 @@ public class HttpJSONExchange {
 		 * 3. Open stream and read server response
 		 * 
 		 */
+        if(fullDomainName ==null){
+            throw new NullPointerException("No URL provided! Please provide one!");//No domain name was provided
+        }
 		JSONObject jsonResponse = null;
 		URL url = null;//a url object to hold a complete URL
 		try {
@@ -112,6 +136,9 @@ public class HttpJSONExchange {
 		 * 3. Open stream and read server response
 		 * 
 		 */
+        if(fullDomainName ==null){
+            throw new NullPointerException("No URL provided! Please provide one!");//No domain name was provided
+        }
 		JSONObject jsonResponse = null;
 		URL url = null;//a url object to hold a complete URL
 		try {
@@ -158,6 +185,9 @@ public class HttpJSONExchange {
 		 * 3. Open input stream and read server response
 		 * 
 		 */
+        if(fullDomainName ==null){
+            throw new NullPointerException("No URL provided! Please provide one!");//No domain name was provided
+        }
 		JSONObject jsonResponse = null;
 		URL url = null;//a url object to hold a complete URL
 		try {
@@ -209,6 +239,11 @@ public class HttpJSONExchange {
 		 * 3. Open input stream and read server response
 		 * 
 		 */
+
+        if(fullDomainName ==null){
+            throw new NullPointerException("No URL provided! Please provide one!");//No domain name was provided
+        }
+
 		JSONObject jsonResponse = null;
 		URL url = null;//a url object to hold a complete URL
 		try {
